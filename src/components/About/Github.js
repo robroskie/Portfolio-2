b4255a84352188329ff50c55bb915a2975685b08
robroskie <lroblesky@gmail.com>
2022-10-01 17:04:17 -0700

    update to github grid color

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -12,7 +12,7 @@ function Github() {
         username="robroskie"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="#b9f584"
         fontSize={16}
       />
     </Row>

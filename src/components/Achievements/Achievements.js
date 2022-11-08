2e10db4ba1a386f8377b4fbf02690962783980c5
robroskie <lroblesky@gmail.com>
2022-11-07 20:44:09 -0800

    Images for achievement section added. styling required

--- src/components/Achievements/Achievements.js
+++ src/components/Achievements/Achievements.js
@@ -1,62 +1,31 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Particle from "../Particle";
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
-//Controls download link 
-import pdf from "../../Assets/../Assets/Luke_Roblesky_CV.pdf";
-
-import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-
-//Control resume displayed on page
-const resumeLink =
-  "https://raw.githubusercontent.com/robroskie/Portfolio-2/master/src/Assets/Luke_Roblesky_CV.pdf";
+import review1 from "../../Assets/Images/review1.jpg";
+import review2 from "../../Assets/Images/review2.jpg";
 
 function Achievements() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
   return (
-    <div>
-      <Container fluid className="resume-section">
-        <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
+    <Container fluid className="achievements-section">
+      <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+        Student <strong className="purple">Testimonials</strong>
+      </h1>
 
-        <Row className="resume">
-          <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-      </Container>
-    </div>
+      <Col
+     
+        style={{width: '60em', paddingTop: "5em", paddingBottom: "5em" }}
+        className="about-img"
+      >
+        <img src={review1} alt="about" className="img-fluid" />
+      </Col>
+      <Col
+     
+        style={{width: '60em', paddingTop: "5em", paddingBottom: "5em" }}
+        className="about-img"
+      >
+        <img src={review2} alt="about" className="img-fluid" />
+      </Col>
+    </Container>
   );
 }
 
